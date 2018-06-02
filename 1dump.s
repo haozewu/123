c50f095b70c33e064f98dd3cbcca1fd32f0dd02f
吴昊泽 <haozewu@outlook.com>
2018-06-02 10:33:34 +0800

    Create 1dump.s

---
+++ 1dump.s
@@ -0,0 +1,6 @@
+ldr r1, [pc, #20]     ;1c     //exe 0x00-explain addr+4--read addr+8, so pc=0x00 +8   gpfcon   0x1c:56000050
+mov r0 ,#256          ;0x100
+str r0, [r1]           ;              //write r0(0x100) to r1(0x1c,56000050)
+ldr r1, [pc, #12]      ;20
+mov r0, #0
+str r0, [r1]
