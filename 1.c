bbf3ac9400820c8175c91816767f804dd9627d7a
吴昊泽 <haozewu@outlook.com>
2018-06-02 11:37:36 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,9 +1,10 @@
-void main()
+int main()
 {
-  unsigned int *pGPFCON=0x56000050;
-  unsigned int *pGPFDAT=0X56000054;
+  unsigned int *pGPFCON=(unsigned int *)0x56000050;
+  unsigned int *pGPFDAT=(unsigned int *)0X56000054;
   
   *pGPFCON=0X100;
   
   *pGPFDAT=0;
+  return 0;
 }
