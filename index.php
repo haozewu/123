f5fdf914a0980a1874e1aab94d99500f66260c57
吴昊泽 <haozewu@outlook.com>
2018-08-24 08:10:45 +0800

    successfully anchor and index of h2h3
    成功使用官方主题Twenty Fifteen搭配Gutenberg实现0X0目录生成以及h2h3标题锚定位，自动列出文章结构。以及代码样式js和css的引入。下一步工作：实现代码的后载入，自主实现默认代码样式的修改。或者简单标注语言后实现完整格式修改，例如有无行号。

--- index.php
+++ index.php
@@ -8,9 +8,36 @@ Author: 夏目秀明
 Author URI: https://www.zetrix.cn
 */
 
-
 // This just echoes the chosen line, we'll position it later
-function hello_dolly($content) {
+function add_anchor($single_text){
+	$all_head2 = preg_split('<h2>', $single_text);
+	$num = 0;
+	foreach ($all_head2 as $each_one) {
+		if($num == 0){
+			$all_text .= $each_one."h2";
+		}else if(($num%2) == 0){
+			$all_text .= $each_one."h2 id=\"header2-".$num."\"";
+		}else{
+			$all_text .= $each_one."h2";
+		}
+		$num += 1;
+	}
+	$all_head3 = preg_split('<h3>', $all_text);
+	$num = 0;
+	foreach ($all_head3 as $each_one) {
+		// if($num == 0){
+		// 	$all_text .= $each_one."h3";
+		// }else 
+		if(($num%2) == 0){
+			$all_texth3 .= $each_one."h3 id=\"header3-".$num."\"";
+		}else{
+			$all_texth3 .= $each_one."h3";
+		}
+		$num += 1;
+	}
+	return $all_texth3;
+}
+function add_index($content) {
 	if(is_single()) {
 		// [^>]表示不是“>”的字符，*表示重复零次或更多次，这个意思是非“>”的字符可以有一个或多个，也可以没有。
 		// 点代表的是任意字符。
@@ -18,44 +45,38 @@ function hello_dolly($content) {
 		$rex = "/(<(h2|h3|h4)>)(.*?)(<\/(h2|h3|h4)>)/";
 		//通配所有h2h23h4
 		preg_match_all($rex, $content, $matches,PREG_SET_ORDER);
+		//生成顺序表
+		$h3_num = 0;
+		$num = 1;
 		foreach ($matches as $val) {
 			if($val[2] == "h2"){
 				//class="has-regular-font-size"
-				$label = '<p class="has-regular-font-size">'.$val[3]."</p>";
+				$label = '<p class="has-regular-font-size"><a href=#header2-'.($num*2).'>'.$val[3]."</a></p>";
 			}
 			if($val[2] == "h3"){
 				//class="has-small-font-size"
-				$label = '<p class="has-small-font-size">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$val[3]."</p>";
+				$label = '<p class="has-small-font-size">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href=#header3-'.($h3_num*2).'>'.$val[3]."</a></p>";
+				$h3_num += 1;
 			}
 			$headers .= $label;
+			$num += 1;
 		}
 		//从第一个h2开始分割，也就是以后的正文第一个就要是h2标签，以0X1开始
 		$moreandh2 = preg_split('<h2>', $content, 2);
-		$content = $moreandh2[0]."h2>0X0 目录</h2>".$headers."<h2".$moreandh2[1];	
+		$content = $moreandh2[0]."h2>0X0 目录</h2>".$headers."<h2".$moreandh2[1];
+		$content = add_anchor($content);
 	  }
 	  return $content;
 }
 // Now we set that function up to execute when the admin_notices action is called
-add_filter( 'the_content', 'hello_dolly' );
+add_filter( 'the_content', 'add_index' );
 
 // We need some CSS to position the paragraph
-function dolly_css() {
-	// This makes sure that the positioning is also good for right-to-left languages
-	$x = is_rtl() ? 'left' : 'right';
-
-	echo "
-	<style type='text/css'>
-	#dolly {
-		float: $x;
-		padding-$x: 15px;
-		padding-top: 5px;
-		margin: 0;
-		font-size: 11px;
-	}
-	</style>
-	";
+function prism_css() {
+	echo '<link rel="stylesheet" href="https://web-share.bj.bcebos.com/css/prism.css" />
+	<script src="https://web-share.bj.bcebos.com/js/prism.js" type="text/javascript"></script>
+	';
 }
-
-add_action( 'admin_head', 'dolly_css' );
+add_action( 'wp_head', 'prism_css' );
 
 ?>
