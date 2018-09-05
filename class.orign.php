5dd25e89154a05bed37c43e7176841eb61a81522
吴昊泽 <haozewu@outlook.com>
2018-09-05 23:35:24 +0800

    place orign function in one class
    把原创声明归类完成，该写doc的markdown了。

---
+++ class.orign.php
@@ -0,0 +1,43 @@
+<?php
+class Orign{
+    /**这段代码实现在保存post的时候执行，连同本参数一并保存 */
+    function save_iforign( $post_id ) {
+        // 安全检查
+        // 检查是否发送了一次性隐藏表单内容（判断是否为第三者模拟提交）
+        if ( ! isset( $_POST['ifpost_orign_nonce'] ) ) {
+            return;
+        }
+        // 判断隐藏表单的值与之前是否相同
+        if ( ! wp_verify_nonce( $_POST['ifpost_orign_nonce'], 'ifpost_orign_meta_box' ) ) {
+            return;
+        }
+        // 判断该用户是否有权限
+        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return;
+        }
+        // 判断 Meta Box 是否为空，要是空的话说明不是原创，这个不能不保存
+        // if ( ! isset( $_POST['if_post_orign'] ) ) {
+        //     return;
+        // }
+        $if_post_orign = $_POST['if_post_orign'];
+        update_post_meta( $post_id, 'if_post_orign', $if_post_orign );
+    }
+    
+
+    //原创整合部分
+    function orign_out($content){
+        $settings = get_option('ciabeta-options');
+        //没有声明global，就显示有问题
+        if($settings['copyright_on'] == 'on'){
+        global $post;
+        $orign_query = get_post_meta($post->ID, 'if_post_orign', true);
+        if($orign_query == 'yes')
+            $content .= $settings['orign'];
+            else {
+                $content .= $settings['not_orign'];
+            }
+        }
+        return $content;
+    }
+
+}
--- index.php
+++ index.php
@@ -105,81 +105,42 @@ add_action('login_enqueue_scripts','login_protection');
 /**
  * 终于到了实现原创功能的时候了，下面的函数是发post的时候的回调函数，输出一个发post的时候的框的内容
  */
- function ifpost_orign($post) {
+function main_save_iforign($post_id){
+	require_once('class.orign.php');
+	$orign = new Orign;
+	return $orign->save_iforign($post_id);
+}
+function main_orign_out($content){
+	require_once('class.orign.php');
+	$orign = new Orign;
+	$content = $orign->orign_out($content);
+	return $content;
+}
+function ifpost_orign($post) {
 	// 创建临时隐藏表单，为了安全
-    wp_nonce_field( 'ifpost_orign_meta_box', 'ifpost_orign_nonce' );
-    // 获取之前存储的值
+	wp_nonce_field( 'ifpost_orign_meta_box', 'ifpost_orign_nonce' );
+	// 获取之前存储的值
 	$if_orign_ok = get_post_meta($post->ID, 'if_post_orign', true);
 	if($if_orign_ok == 'yes')
 		$checked_ok = ' checked="checked"';
 	//这里不要瞎用同一个参数！！
 	echo '<span style="padding-top:5px;display:block;"><input type="checkbox" value="yes" name="if_post_orign"'.$checked_ok.' />申明为原创</span>';
 }
-/**这段代码实现在保存post的时候执行，连同本参数一并保存 */
-function save_iforign( $post_id ) {
-		//这里调试一下看一下参数
-		// require_once('class.debug.php');
-    	// $mydebug = new MyDebug;
-        // $mydebug->console_log('on');
-		// 这里保存的参数就是post的_value吧
-		// 安全检查
-    // 检查是否发送了一次性隐藏表单内容（判断是否为第三者模拟提交）
-    if ( ! isset( $_POST['ifpost_orign_nonce'] ) ) {
-        return;
+    /**
+     * 这个是控制post的那个框的实现，post和high有机会查查是什么用
+     */
+     function iforign_meta_box() {
+        if ( function_exists('add_meta_box') ) {
+            /**判断是否有add_meta_box函数，这个不是系统函数么，有什么意义么？
+             * 我知道这个的函数了，是等待有post被编辑的时候实现。
+             * 第一个参数是box的id号
+             */
+            add_meta_box( 'the-only-id', '原创申明', 'ifpost_orign', 'post', 'side', 'high' );
+        }
     }
-    // 判断隐藏表单的值与之前是否相同
-    if ( ! wp_verify_nonce( $_POST['ifpost_orign_nonce'], 'ifpost_orign_meta_box' ) ) {
-        return;
-    }
-    // 判断该用户是否有权限
-    if ( ! current_user_can( 'edit_post', $post_id ) ) {
-        return;
-    }
- 
-    // 判断 Meta Box 是否为空，要是空的话说明不是原创，这个不能不保存
-    // if ( ! isset( $_POST['if_post_orign'] ) ) {
-    //     return;
-    // }
-    $if_post_orign = $_POST['if_post_orign'];
-    update_post_meta( $post_id, 'if_post_orign', $if_post_orign );
-}
-add_action('save_post', 'save_iforign');
-/**
- * 这个是控制post的那个框的实现，post和high有机会查查是什么用
- */
-function iforign_meta_box() {
-	if ( function_exists('add_meta_box') ) {
-		/**判断是否有add_meta_box函数，这个不是系统函数么，有什么意义么？
-		 * 我知道这个的函数了，是等待有post被编辑的时候实现。
-		 * 第一个参数是box的id号
-		 */
-		add_meta_box( 'the-only-id', '原创申明', 'ifpost_orign', 'post', 'side', 'high' );
-	}
-}
+add_action('save_post', 'main_save_iforign');
 add_action('admin_menu', 'iforign_meta_box');
-//原创整合部分
-function orign_out($content){
-
-		 $settings = get_option('ciabeta-options');
-		 //没有声明global，就显示有问题
-		 if($settings['copyright_on'] == 'on')
-		 {
-			global $post;
-		 
-		 $orign_query = get_post_meta($post->ID, 'if_post_orign', true);
-		 if($orign_query == 'yes')
-			 $content .= $settings['orign'];
-			 else {
-				 $content .= $settings['not_orign'];
-			 }
-
-		 }
-		 
-
-		 return $content;
-
-}
-add_filter( 'the_content', 'orign_out');
+add_filter( 'the_content', 'main_orign_out');
 /**
  * 加一个后台菜单玩玩
  * 当点击menu page时，执行本指令
@@ -191,8 +152,7 @@ function get_options(){
 	include( 'structure/options.php' );
 }
 function ciabeta_options(){
-	
-	add_menu_page( '123','Ciabeta-Panel', 'manage_options', 'Ciabeta-Panel',  'get_options', plugins_url( 'img/ciabeta.png', __FILE__ ));
+	add_menu_page( 'Ciabeta-Panel','Ciabeta-Panel', 'manage_options', 'Ciabeta-Panel',  'get_options', plugins_url( 'img/ciabeta.png', __FILE__ ));
 }
 add_action('admin_menu',  'ciabeta_options'  );
 
